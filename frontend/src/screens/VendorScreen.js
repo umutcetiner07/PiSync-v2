de64ce06db174674076d52e5e4c0adcd02e12c6c
Umut Çetiner <umutcetiner@gmail.com>
2025-08-17 16:03:32 +0300

    Create VendorScreen.js

---
+++ frontend/src/screens/VendorScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function VendorScreen() {
+  return (
+    <View style={{ flex:1, justifyContent:'center', alignItems:'center' }}>
+      <Text>Vendors List (Demo)</Text>
+    </View>
+  );
+}
