46c768f7669ed426035fe3ccf716720fdeae946a
Umut Çetiner <umutcetiner@gmail.com>
2025-08-17 16:05:07 +0300

    Create CompareScreen.js

---
+++ frontend/src/screens/CompareScreen.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList } from 'react-native';
+
+export default function CompareScreen() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/products')
+      .then(res => res.json())
+      .then(data => setProducts(data))
+      .catch(e => console.log(e));
+  }, []);
+
+  return (
+    <View style={{ flex:1, justifyContent:'center', alignItems:'center', padding:20 }}>
+      <Text style={{ fontSize:18, fontWeight:'bold', marginBottom:10 }}>Price Comparison</Text>
+      <FlatList 
+        data={products}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({ item }) => (
+          <Text>{item.name} - {item.vendor} - {item.price}</Text>
+        )}
+      />
+    </View>
+  );
+}
