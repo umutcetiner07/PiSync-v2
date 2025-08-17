1ae413ec3d9dce06daba383f3b59a56481490879
Umut Çetiner <umutcetiner@gmail.com>
2025-08-17 16:20:05 +0300

    remove duplicate HomeScreen.js

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,10 +1,1 @@
-import React from 'react';
-import { View, Button } from 'react-native';
-export default function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex:1, justifyContent:'center', alignItems:'center' }}>
-      <Button title="View Vendors" onPress={() => navigation.navigate('Vendors')} />
-      <Button title="Compare Prices" onPress={() => navigation.navigate('Compare')} />
-    </View>
-  );
-}
+
