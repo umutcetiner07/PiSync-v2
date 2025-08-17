42432ac368536af7f9ff12f011f1cac23e02cffb
Umut Çetiner <umutcetiner@gmail.com>
2025-08-17 16:22:54 +0300

    remove duplicate VendorScreen.js

--- VendorScreen.js
+++ VendorScreen.js
@@ -1,7 +1,1 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-export default function VendorScreen() {
-  return (
-    <View><Text>Vendors List (Demo)</Text></View>
-  );
-}
+
